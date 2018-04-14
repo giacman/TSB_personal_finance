756b9bedb6bb7ca96593918a4c7fbe7d4a84cf37
Giacomo Vannucchi <giacomo.vannucchi@akqa.com>
2018-04-14 22:06:34 +0100

    adding csv to gitignore and adding setup script

--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 chooseCRANmirror(graphics=FALSE, ind=1)
 
-install.packages(c("shiny", "rstudioapi","dplyr","lubridate","ggplot2","plotly","tidyr"))
+install.packages(c("shiny", "rstudioapi","dplyr","lubridate","ggplot2","plotly","tidyr","DT"))
